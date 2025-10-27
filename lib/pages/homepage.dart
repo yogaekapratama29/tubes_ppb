547ccc962f912dad708da73a1d075b1150c82a03
yogaekapratama29 <yogaex29@gmail.com>
2025-10-27 09:45:13 +0700

    + add new intial

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -12,61 +12,68 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.asset("assets/bg.jpeg", fit: BoxFit.cover),
-          ),
-          SafeArea(
-            child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    // Konten Atas
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "Arbtilant",
-                          style: TextStyle(
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w800,
-                            fontSize: 40,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(width: 100),
-                        Container(
-                          height: 50,
-                          width: 50,
-                          child: Icon(Icons.search, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 15, 191, 29),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        SizedBox(width: 20),
-                        Container(
-                          height: 50,
-                          width: 50,
-                          child: Icon(Icons.share, color: Colors.white),
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 15, 191, 29),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ],
-                    ),
-                    
-                  ],
-                ),
-              ),
-            ),
+      bottomNavigationBar: BottomNavigationBar(backgroundColor: Color.fromARGB(255, 15, 191, 29),
+        items: [
+          BottomNavigationBarItem(icon: Icon(color: Colors.white,Icons.home), label: "Home",),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner,color: Colors.white),
+            label: "Scan",
           ),
         ],
+      ),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                // Konten Atas
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Arbtilant",
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w800,
+                        fontSize: 40,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 15, 191, 29),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: Icon(Icons.share, color: Colors.white),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 15, 191, 29),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 50),
+                Container(
+                  height: 400,
+                  width: 1000,
+                  color: Color.fromARGB(255, 15, 191, 29),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
